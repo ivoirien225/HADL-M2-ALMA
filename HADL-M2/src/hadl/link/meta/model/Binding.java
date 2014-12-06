05fb0d6aa57a448211a825fd77ab9f00d20dc277
gyann972 <grego.yannis@gmail.com>
2014-12-06 14:01:52 +0100

    Class implementation of package "hadl.link.meta.model".

--- HADL-M2/src/hadl/link/meta/model/Binding.java
+++ HADL-M2/src/hadl/link/meta/model/Binding.java
@@ -1,5 +1,12 @@
 package hadl.link.meta.model;
 
 public class Binding extends Link {
-
+	
+	
+	public Binding(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
--- HADL-M2/src/hadl/link/meta/model/Link.java
+++ HADL-M2/src/hadl/link/meta/model/Link.java
@@ -1,5 +1,13 @@
 package hadl.link.meta.model;
 
 abstract class Link {
+	private String name;
 
+	public Link(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
+	
 }
