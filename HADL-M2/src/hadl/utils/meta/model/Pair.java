635d3a99429f32961699377b61479d8c5abd44a6
gyann972 <grego.yannis@gmail.com>
2014-12-07 17:15:05 +0100

    Adding of a method "get" to Pair class.

--- HADL-M2/src/hadl/utils/meta/model/Pair.java
+++ HADL-M2/src/hadl/utils/meta/model/Pair.java
@@ -22,4 +22,22 @@ public class Pair {
 	public void setSecondObject(Object secondObject) {
 		this.secondObject = secondObject;
 	}
+	
+	/**Permit to obtain object associated to a object.
+	 * To achieve this the methode need to the object couple.
+	 * 
+	 * @param obj 
+	 * @param couple Containing the object associated to obj 
+	 * @return The associated object of obj
+	 */
+	public static Object get(Object obj , Pair couple){
+		
+		if(couple.firstObject.equals(obj)){
+			return couple.secondObject;
+		}
+		else {
+			return couple.firstObject;
+		}
+		
+	}
 }
