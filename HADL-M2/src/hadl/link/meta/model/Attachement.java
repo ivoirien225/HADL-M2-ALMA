29b694de4030a01f7d711c9713dae115e9376389
gyann972 <grego.yannis@gmail.com>
2014-12-07 15:30:22 +0100

    Refactoring of Attachment constructor.

--- HADL-M2/src/hadl/link/meta/model/Attachement.java
+++ HADL-M2/src/hadl/link/meta/model/Attachement.java
@@ -4,7 +4,9 @@ import hadl.utils.meta.model.Pair;
 
 public class Attachement extends Link {
 	Pair interfaces;
-	public Attachement(String name) {
+
+	public Attachement(String name, Pair interfaces) {
 		super(name);
+		this.interfaces = interfaces;
 	}
 }
