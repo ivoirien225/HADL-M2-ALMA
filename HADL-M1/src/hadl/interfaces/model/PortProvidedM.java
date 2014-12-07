ab4c8464469319c01d13bf6584fb9dbc88c1a7ca
gyann972 <grego.yannis@gmail.com>
2014-12-07 20:17:54 +0100

    Adding of package "hadl.interfaces.model" and its respective class.

---
+++ HADL-M1/src/hadl/interfaces/model/PortProvidedM.java
@@ -0,0 +1,39 @@
+package hadl.interfaces.model;
+
+import hadl.interfaces.meta.model.PortProvided;
+import hadl.tools.interfaces.Observable;
+import hadl.tools.interfaces.Observer;
+import hadl.tools.message.Message;
+
+public class PortProvidedM extends PortProvided implements Observable,Observer {
+
+	public PortProvidedM(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(Observable observable, Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyObservers(Observable observable, Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addObserver(Observer ob) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteObserver(Observer ob) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ HADL-M1/src/hadl/interfaces/model/PortRequiredM.java
@@ -0,0 +1,43 @@
+package hadl.interfaces.model;
+
+import java.util.List;
+
+import hadl.interfaces.meta.model.PortRequired;
+import hadl.tools.interfaces.Observable;
+import hadl.tools.interfaces.Observer;
+import hadl.tools.message.Message;
+
+public class PortRequiredM extends PortRequired implements Observable,Observer{
+
+	List<Observer> observers;
+	
+	public PortRequiredM(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void notifyObservers(Observable observable, Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addObserver(Observer ob) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteObserver(Observer ob) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable observable, Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
