4359841e906eeb980df9686324a3858c7f67cbf7
gyann972 <grego.yannis@gmail.com>
2014-12-07 16:21:46 +0100

    Update of notifyObservers method signature.

--- HADL-M1/src/hadl/tools/interfaces/Observable.java
+++ HADL-M1/src/hadl/tools/interfaces/Observable.java
@@ -1,7 +1,9 @@
 package hadl.tools.interfaces;
 
+import hadl.tools.message.Message;
+
 public interface Observable {
-	public void notifyObservers();
+	public void notifyObservers(Observable observable, Message message);
 	public void addObserver(Observer ob);
 	public void deleteObserver(Observer ob);
 }
