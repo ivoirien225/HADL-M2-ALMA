efbf378b81a2cff370b52a5943ae3e5ef2f6961a
Cyrille <agnesgnagne@gmail.com>
2014-12-07 17:39:12 +0100

    adding print method and body to client super methods

--- HADL-M1/src/hadl/component/model/Client.java
+++ HADL-M1/src/hadl/component/model/Client.java
@@ -1,9 +1,60 @@
 package hadl.component.model;
 
-public class Client {
+import java.util.List;
+import hadl.component.meta.model.SimpleComponent;
+import hadl.interfaces.meta.model.PortProvided;
+import hadl.interfaces.meta.model.PortRequired;
+import hadl.interfaces.meta.model.ServiceProvided;
+import hadl.interfaces.meta.model.ServiceRequired;
+import hadl.tools.interfaces.Observable;
+import hadl.tools.interfaces.Observer;
+import hadl.tools.message.Message;
 
-	public Client() {
-		// TODO Auto-generated constructor stub
+public class Client extends SimpleComponent implements Observable, Observer{
+	
+	List<Observer> listObserver;
+	Message  response; 
+	public Client(String name, List<PortProvided> portProvided,
+			List<PortRequired> portRequired,
+			List<ServiceProvided> serviceProvided,
+			List<ServiceRequired> required) {
+		super(name, portProvided, portRequired, serviceProvided, required);
 	}
 
+	@Override
+	public void update(Observable observable, Message message){
+		if(observable instanceof PortRequired){
+			this.response = message;
+			afficheReponse(message);
+		}
+		else{
+			logExceptionOnConsol("update");
+		}
+	}
+	
+
+	@Override
+	public void notifyObservers(Observable observable, Message message) {
+		for(Observer o : listObserver){
+			o.update(this, message);
+		}
+	}
+
+	@Override
+	public void addObserver(Observer ob) {
+		this.listObserver.add(ob);
+	}
+
+	@Override
+	public void deleteObserver(Observer ob) {
+		this.listObserver.remove(ob);
+	}
+	
+	public void afficheReponse(Message response){
+		System.out.println("La reponse à la requête "+response.getName()+" est: "+response.getName()+response.getResponse());	
+	}
+	
+	public void logExceptionOnConsol(String methode){
+		System.out.println("Erreur à l'appel de la méthode :"+methode);
+	}
 }
