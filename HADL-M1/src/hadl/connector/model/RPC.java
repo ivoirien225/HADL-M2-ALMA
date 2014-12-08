5396fc35ae70d5b7296d5e67a677aff387be05a1
gyann972 <grego.yannis@gmail.com>
2014-12-08 12:42:15 +0100

    Implementation of RPC connector.

--- HADL-M1/src/hadl/connector/model/RPC.java
+++ HADL-M1/src/hadl/connector/model/RPC.java
@@ -1,8 +1,16 @@
 package hadl.connector.model;
 
-public class RPC {
+import hadl.connector.meta.model.Glue;
+import hadl.interfaces.meta.model.Role_Provide;
+import hadl.interfaces.meta.model.Role_Required;
 
-	public RPC() {
+import java.util.List;
+
+public class RPC extends SimpleConnectorOO{
+
+	public RPC(String name, List<Role_Provide> listRole_Provide,
+			List<Role_Required> listRole_Required, Glue glue) {
+		super(name, listRole_Provide, listRole_Required, glue);
 		// TODO Auto-generated constructor stub
 	}
 
