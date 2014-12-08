03317189aba44941266029347079daeb622de27b
gyann972 <grego.yannis@gmail.com>
2014-12-08 11:43:30 +0100

    Adding of getter and new constructor for Glue class.

--- HADL-M2/src/hadl/connector/meta/model/Glue.java
+++ HADL-M2/src/hadl/connector/meta/model/Glue.java
@@ -5,11 +5,22 @@ import java.util.List;
 
 import hadl.utils.meta.model.Pair;
 
-public class Glue{
+public class Glue {
 	
+	public String getNameS() {
+		return nameS;
+	}
+
+	public List<Pair> getPairList() {
+		return pairList;
+	}
+
+	String nameS = ""; 
 	List<Pair> pairList = new ArrayList<Pair>();
-	public Glue(List<Pair> pairList) {
+	
+	public Glue(String nameS, List<Pair> pairList) {
 		super();
+		this.nameS = nameS;
 		this.pairList = pairList;
 	}
 	
