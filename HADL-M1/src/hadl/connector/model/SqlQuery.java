7e7b80c354fa4187ad495d7c33002a7516343769
gyann972 <grego.yannis@gmail.com>
2014-12-08 14:17:02 +0100

    Adding of Connector SqlQuery.

---
+++ HADL-M1/src/hadl/connector/model/SqlQuery.java
@@ -0,0 +1,17 @@
+package hadl.connector.model;
+
+import hadl.connector.meta.model.Glue;
+import hadl.interfaces.meta.model.Role_Provide;
+import hadl.interfaces.meta.model.Role_Required;
+
+import java.util.List;
+
+public class SqlQuery extends SimpleConnectorOO {
+
+	public SqlQuery(String name, List<Role_Provide> listRole_Provide,
+			List<Role_Required> listRole_Required, Glue glue) {
+		super(name, listRole_Provide, listRole_Required, glue);
+		// TODO Auto-generated constructor stub
+	}
+
+}
