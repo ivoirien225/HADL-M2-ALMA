fa27087003b9bd667f7676d1c81cfd1f1dc4eb1d
Cyrille <agnesgnagne@gmail.com>
2014-12-10 17:30:27 +0100

    clean of logger class

--- HADL-M1/src/hadl/utils/Logger.java
+++ HADL-M1/src/hadl/utils/Logger.java
@@ -5,10 +5,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import hadl.component.meta.model.Component;
-import hadl.connector.meta.model.SimpleConnector;
-import hadl.interfaces.meta.model.Interface;
 import hadl.interfaces.meta.model.PortProvided;
 
 public class Logger {
@@ -24,74 +20,7 @@ public class Logger {
 		}
 		return res.toString();
 	}
-/*	
-	public static void loggerExceptionWritter(Component component, String methodName,  Message message) throws IOException {
-		System.out.println("[INFOS]**[EXCEPTION] -- Element name: " + component.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response);
-		String chaine = loggerReader();
-		FileWriter writer = new FileWriter(new File("Repository/log.txt"));
-		if(!(chaine.equals(null))){
-			chaine = chaine + "[INFOS] -- Element name: " + component.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response; 
-		}
-		writer.write(chaine);
-		writer.close();
-	}
-	
-	public static void loggerExceptionWritter(Interface interfaceCom, String methodName, Message message) throws IOException {
-		System.out.println("[INFOS]**[EXCEPTION] -- Element name: " + interfaceCom.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response);
-		String chaine = loggerReader();
-		FileWriter writer = new FileWriter(new File("Repository/log.txt"));
-		if(!(chaine.equals(null))){
-			chaine = chaine + "[INFOS] -- Element name: " + interfaceCom.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response; 
-		}
-		writer.write(chaine);
-		writer.close();
-	}
-	
-	public static void loggerWritter(Interface interfaceCom, String methodName, Message message) throws IOException {
-		System.out.println("[INFOS] -- Element name: " + interfaceCom.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response);
-		String chaine = loggerReader();
-		FileWriter writer = new FileWriter(new File("Repository/log.txt"));
-		if(!(chaine.equals(null))){
-			chaine = chaine + "[INFOS] -- Element name: " + interfaceCom.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response; 
-		}
-		writer.write(chaine);
-		writer.close();
-	}
-		
-	
-	public static void loggerWritter(Component component, String methodName,  Message message) throws IOException {
-		System.out.println("[INFOS] -- Element name: " + component.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response);
-		String chaine = loggerReader();
-		FileWriter writer = new FileWriter(new File("Repository/log.txt"));
-		if(!(chaine.equals(null))){
-			chaine = chaine + "[INFOS] -- Element name: " + component.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response; 
-		}
-		writer.write(chaine);
-		writer.close();
-	}
-	
-	public static void loggerWritter(SimpleConnector simpleConnector, String methodName,  Message message) throws IOException {
-		System.out.println("[INFOS] -- Element name: " + simpleConnector.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response);
-		String chaine = loggerReader();
-		FileWriter writer = new FileWriter(new File("Repository/log.txt"));
-		if(!(chaine.equals(null))){
-			chaine = chaine + "[INFOS] -- Element name: " + simpleConnector.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response; 
-		}
-		writer.write(chaine);
-		writer.close();
-	}
-	
-	public static void loggerExceptionWritter(SimpleConnector simpleConnector, String methodName,  Message message) throws IOException {
-		System.out.println("[INFOS]**[EXCEPTION] -- Element name: " + simpleConnector.getClass().getSimpleName() + "Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response);
-		String chaine = loggerReader();
-		FileWriter writer = new FileWriter(new File("Repository/log.txt"));
-		if(!(chaine.equals(null))){
-			chaine = chaine + "[INFOS] -- Element name: " + simpleConnector.getClass().getSimpleName() + "Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response; 
-		}
-		writer.write(chaine);
-		writer.close();
-	}
-	*/
+
 	public static void loggerWritter(Object ob, String methodName,  Message message) throws IOException {
 		System.out.println("[INFOS] -- Element name: " + ob.getClass().getSimpleName() + " Method called: "+ methodName + " " + " Message type: " + message.getName() + " "+ "Message result: " + message.response);
 		String chaine = loggerReader();
